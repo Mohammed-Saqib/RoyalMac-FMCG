34f478ce1363d5ef6a761a4da2f9897eecf29d97
Mohammed Saqib <mohammedsaqibbca62@gmail.com>
2022-05-19 16:02:56 +0530

    go up position testing

--- scripts/main.js
+++ scripts/main.js
@@ -110,3 +110,15 @@ $(document).ready(function () {
         }
     });
 });
+
+
+// <!--Start of Tawk.to Script-->
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/628600827b967b11799027d5/1g3dn3vjo';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
