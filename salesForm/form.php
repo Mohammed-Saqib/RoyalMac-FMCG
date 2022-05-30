be88022710774975b31ca71a87f091dd0e8d13af
Mohammed Saqib <mohammedsaqibbca62@gmail.com>
2022-05-31 00:49:16 +0530

    added product quote form code for each products

--- salesForm/form.php
+++ salesForm/form.php
@@ -15,4 +15,26 @@ $body .= "Message: ".$message. "\r\n";
 mail($to,$subject,$body);
 header('location:../index.html');
 }
+
+if(isset($_POST['pName'], $_POST['pCompanyName'], $_POST['pEmail'], $_POST['pNumber'], $_POST['requirements'], $_POST['sCountry'], $_POST['pPhoneCode']))
+{
+$name = $_POST['pName'];
+$companyName = $_POST['pCompanyName'];
+$country = $_POST['sCountry'];
+$phoneNumber = $_POST['phoneCode'] . ' ' . $_POST['phoneNumber'];
+$email = $_POST['pEmail'];
+$requirements = $_POST['requirements'];
+
+$to = "sales@royalmacinternational.com";
+$body = "";
+
+$body .= "From: ".$name. "\r\n";
+$body .= "Company Name: ".$companyName. "\r\n";
+$body .= "Country: ".$country. "\r\n";
+$body .= "Phone Number: ".$phoneNumber. "\r\n";
+$body .= "Email: ".$email. "\r\n";
+$body .= "Requirements: ".$requirements. "\r\n";
+mail($to,$subject,$body);
+header('location:../index.html');
+}
 ?>
