0aa70b06e81ed4ad2517ea7694415498f6657699
Mohammed Saqib <mohammedsaqibbca62@gmail.com>
2022-06-20 15:55:27 +0530

    nav button svg changed to  span in all the pages

--- scripts/productQuote.js
+++ scripts/productQuote.js
@@ -1,16 +1,15 @@
 let navToggle = false;
 let menuList = document.querySelector('.info-nav');
 let navButton = document.querySelector('.info-nav-btn');
-let navLogo = document.getElementById('info-menu-icon')
 navButton.addEventListener('click', function (e) {
     e.preventDefault();
     navToggle = !navToggle;
     if(navToggle){
         menuList.classList.add('active-nav');
-        navLogo.innerHTML = '<path d="M10 8.586L2.929 1.515L1.515 2.929L8.586 10L1.515 17.071L2.929 18.485L10 11.414L17.071 18.485L18.485 17.071L11.414 10L18.485 2.929L17.071 1.515L10 8.586V8.586Z" fill="white"/>'
+        navButton.classList.add('info-nav-btn-active')
     }else{
-        navLogo.innerHTML = '<path d="M0 3H20V5H0V3ZM0 9H20V11H0V9ZM0 15H20V17H0V15Z" fill="white"/>';
         document.querySelector('.active-nav') ? menuList.classList.remove('active-nav') : " ";
+        document.querySelector('.info-nav-btn-active') ? navButton.classList.remove('info-nav-btn-active') : " ";
     }
 })
 
